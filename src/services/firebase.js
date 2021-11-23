8b13513cdb7a2829c52ad903ca114763d3420bae
Vlad <work.ed1@yandex.ru>
2021-11-23 18:47:49 +0300

    добавлен firebase

---
+++ src/services/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyABJB8g_-rFtuqwk4PpeJdPW6erGSSjtNQ",
+  authDomain: "gb-react-messenger.firebaseapp.com",
+  projectId: "gb-react-messenger",
+  storageBucket: "gb-react-messenger.appspot.com",
+  messagingSenderId: "279432002151",
+  appId: "1:279432002151:web:e039701efd37e4e705f2c2",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const db = firebase.database();
+
+export const rootRef = db.ref("root");
+
+export const chatsRef = rootRef.child("chats");
